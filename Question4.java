ad4e852ba60c6eca4fab5a8dcb0cfd6ec2bf63c1
filora119 <80975616+filora119@users.noreply.github.com>
2021-04-29 00:15:47 +0200

    Update Question4.java

--- Question4.java
+++ Question4.java
@@ -6,34 +6,25 @@ public class Question4 {
 
 	public static void main(String[] args) {
 
-		/* Scanner sc = new Scanner(System.in);
-		 System.out.println("Enter number");
-		 int n=sc.nextInt();*/
-		     Object a=1;
-			
-			display(7);
-	    
-		
-		
-		
-		
-	}
-	public static void display(int n)
-    {
-    // Outer for loop for number of lines
-    for (int i = 0; i<=n; i++) {
-    // Inner for loop for logic execution
-    for (int j = 0; j<= n / 2; j++) {
-    // prints two column lines
-    if ((j == 0 || j == n / 2) && i != 0 ||
-    // print first line of alphabet
-    i == 0  && j != n / 2 ||
-    // prints middle line
-    i == n / 2)
-    System.out.print("*");
-    else
-    System.out.print(" ");
-    }
-    System.out.println();
+	String star = "";
+
+		for (int row = 0; row < num; row++) {
+			if (row == 0 || row == num - 1) {
+				for (int i = 0; i < num - 1; i++) {
+					star += "*";
+				}
+				System.out.print(star);
+				star = "";
+			} else {
+				System.out.print("*");
+			}
+			for (int col = 0; col < num - 2; col++) {
+				if (!(row == 0 || row == num - 1)) {
+					System.out.print(" ");
+				}
+
+			}
+
+			System.out.println("*");
     }
 }}
